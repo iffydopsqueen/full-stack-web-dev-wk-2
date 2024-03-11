e017d1108b14a55f4286159f2791a632d66061e2
Iffy Eleweke <omaleweks@gmail.com>
2024-03-11 17:43:01 -0400

    Updated the render() method to render the form

--- Day 1/m5-w2-d1-exercise/demo1.js
+++ Day 1/m5-w2-d1-exercise/demo1.js
@@ -18,11 +18,14 @@ class NameForm extends React.Component {
       event.preventDefault();
     }
     
-  
     render() {
       return (
         <form onSubmit={this.handleSubmit}>
-          
+          <label>
+            Name:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
         </form>
       );
     }
