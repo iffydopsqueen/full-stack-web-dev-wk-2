eeca88619599a466611a74be8fd3dab023320403
Iffy Eleweke <omaleweks@gmail.com>
2024-03-13 13:13:37 -0400

    Added the Routes and useParams

--- Day 2/m5-w2-d2-demo/demo-app/src/index.js
+++ Day 2/m5-w2-d2-demo/demo-app/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 //import App from './App';
-import App from './routerexample1';
+// import App from './routerexample1';
+import App from './routerexample2';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- Day 2/m5-w2-d2-demo/demo-app/src/routerexample2.js
+++ Day 2/m5-w2-d2-demo/demo-app/src/routerexample2.js
@@ -1,30 +1,72 @@
 import React from "react";
 import {
-  
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  useParams
 } from "react-router-dom";
 
 export default function App() {
   return (
-      <Router>
-        <div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/topics">Topics</Link>
-            </li>
-          </ul>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
 
-          <Routes>
-            <Route path="/about" element={ <About />} />
-            <Route path="/topics" element={<Topics />} />
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/topics" element={<Topics />}>
+            <Route path=":topicId" element={<Topic />} />
+            <Route path="*" element={<h3>Please select a topic.</h3>} />
+          </Route>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </Router>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Topics() {
+  return (
+    <div>
+      <h2>Topics</h2>
+
+      <ul>
+        <li>
+          <Link to="cats">Cats</Link>
+        </li>
+        <li>
+          <Link to="dogs">Dogs</Link>
+        </li>
+      </ul>
+      <Routes>
+        {/* Nested routes */}
+        <Route path=":topicId" element={<Topic />} />
+        <Route path="*" element={<h3>Please select a topic.</h3>} />
+      </Routes>
+    </div>
+  );
+}
+
+function Topic() {
+  let { topicId } = useParams();
+  return <h3>Requested topic ID: {topicId}</h3>;
 }
