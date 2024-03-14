0b6dc582848ab3f5a5a3226aac0faf6560a79f9b
Iffy Eleweke <omaleweks@gmail.com>
2024-03-14 15:07:20 -0400

    Added the 'ratings' property to the product data in the 'products.js' file

--- Day 3/shopcart/src/products.js
+++ Day 3/shopcart/src/products.js
@@ -3,25 +3,29 @@ const productsData = [
       id: 1,
       image: "./products/cologne.jpg",
       desc: "Unisex Cologne",
-      value: 0
+      value: 0,
+      ratings: "4.5/5"    // Added ratings property
     },
     {
       id: 2,
       image: "./products/iwatch.jpg",
       desc: "Apple iWatch",
-      value: 0
+      value: 0,
+      ratings: "3.5/5"
     },
     {
       id: 3,
       image: "./products/mug.jpg",
       desc: "Unique Mug",
-      value: 0
+      value: 0,
+      ratings: "4.0/5"
     },
     {
       id: 4,
       image: "./products/wallet.jpg",
       desc: "Mens Wallet",
-      value: 0
+      value: 0,
+      ratings: "4.5/5"
     }
 ];
 
