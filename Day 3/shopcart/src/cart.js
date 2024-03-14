20078c0fb9b3793e3f3ea628faa4f679137b8096
Iffy Eleweke <omaleweks@gmail.com>
2024-03-14 18:19:48 -0400

    Styled the carts page

--- Day 3/shopcart/src/cart.js
+++ Day 3/shopcart/src/cart.js
@@ -1,14 +1,22 @@
 import React from "react";
 
 const Cart = ({ products }) => {
+
+    // Filter out products with quantity value 0 or below
+    const cartItems = products.filter(product => product.value > 0);
+
     return (
         <div>
-           <h2 className="cart-heading">Your Cart Items</h2>
+           <h3 className="cart-heading">Your Cart Items</h3>
             <div className="cart">
                 <ul>
-                    {products.map((product) => (
-                        <li key={product.id}>
-                            {product.desc} - Quantity: {product.value}
+                    {cartItems.map((product) => (
+                        <li key={product.id} className="cart-item">
+                            <img src={product.image} alt={product.desc} className="cart-item-image" />
+                            <div className="cart-item-description">
+                                {product.desc}
+                            </div>
+                            <span className="cart-item-quantity">Quantity: {product.value}</span>
                         </li>
                     ))}
                 </ul>
