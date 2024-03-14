0133805867dad64109c443a36200576edf63d04c
Iffy Eleweke <omaleweks@gmail.com>
2024-03-14 15:53:35 -0400

    Modified the styling on the cart page

--- Day 3/shopcart/src/cart.js
+++ Day 3/shopcart/src/cart.js
@@ -2,15 +2,17 @@ import React from "react";
 
 const Cart = ({ products }) => {
     return (
-        <div className="cart">
-            <h2>Your Cart Items</h2>
-            <ul>
-                {products.map((product) => (
-                    <li key={product.id}>
-                        {product.desc} - Quantity: {product.value}
-                    </li>
-                ))}
-            </ul>
+        <div>
+           <h2 className="cart-heading">Your Cart Items</h2>
+            <div className="cart">
+                <ul>
+                    {products.map((product) => (
+                        <li key={product.id}>
+                            {product.desc} - Quantity: {product.value}
+                        </li>
+                    ))}
+                </ul>
+            </div> 
         </div>
     );
 };
